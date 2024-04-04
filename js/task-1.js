ecf7d71cb7dcc64ffeb0bbce1f8505c8fbc2c065
DenysMurin <denys.murin.it@gmail.com>
2024-04-04 15:39:53 +0300

    end

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+function slugify(title) {
+  const little = title.toLowerCase();
+  const slugified = little.replaceAll(" ", "-");
+  return slugified;
+}
+
+// второй метод
+function slugify(title) {
+  const titleLowered = title.toLowerCase();
+  const titleSplitted = titleLowered.split(" ");
+  const titleJoined = titleSplitted.join("-");
+  return titleJoined;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
